eacc090cadb1118eea42ca9200c137aa9dbba78d
Stu Andrews <35388073+StuAndrews@users.noreply.github.com>
2018-09-04 12:36:44 +1000

    Paperform Added
    Old body removed. Paperform "full screen embed" code added.

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,15 +9,7 @@ export default ({data}) => (
             <meta name="description" content="The best tech byte for your business"/>
             <meta name="keywords" content="business technology, technology solutions, it projects, systems review, problem solving"/>
         </Helmet>
-        <h1>Code Biscuit</h1>
-        <em>{data.site.siteMetadata.description}</em>
-        <p></p>
-        <p>
-            Hi there. Hope you are well.
-        </p>
-        <p>At Code Biscuit we provide you with the technology smarts to <strong>achieve your goals</strong>, <strong>realise your dreams</strong> and <strong>turn your ideas into reality</strong>.</p>
-        <p>In other words we help you <strong>build and maintain your projects</strong>, <strong>develop solutions to existing problems</strong> and provide <strong>analysis</strong>, <strong>review</strong>, <strong>recommendations</strong> (ways forward) and <strong>reporting</strong> on your business technology processes.</p>
-        <p>A critical element of the service that we provide is to stay abreast of the rapidly evolving technology world. We stay up-to-date with the most effective and efficient advancements and enhancements that come with this change.</p>
+        <div data-paperform-id="mmgyqlli" data-takeover="1"></div><script>(function() { var script = document.createElement('script'); script.src = "https://paperform.co/__embed";document.body.appendChild(script); })()</script>
     </div>
 );
 
