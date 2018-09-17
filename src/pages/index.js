c803d12000edf4aa2f107599530e7852de918af8
Stu Andrews <35388073+StuAndrews@users.noreply.github.com>
2018-09-17 14:33:07 +1000

    Image cleanup
    Moving images into the gatsby file structure.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,8 @@
-import React from "react"
-import Helmet from 'react-helmet'
-import Script from 'react-load-script'
+import React from "react";
+import Helmet from 'react-helmet';
+import Script from 'react-load-script';
+import imageOne from './helloquence-61189-unsplash.jpg';
+import imageTwo from './james-pond-185593-unsplash.jpg';
 
 let test = '<script type="text/javascript">(function() { var script = document.createElement("script"); script.src = "https://paperform.co/__embed";document.body.appendChild(script); })()</script>';
 let subTitleStyle = {
@@ -21,11 +23,11 @@ export default ({data}) => (
         <p>At Code Biscuit we provide you with the technology smarts to <strong>achieve your goals</strong>, <strong>realise your dreams</strong> and <strong>turn your ideas into reality</strong>.</p>
         <p>In othing words we help you <strong>build and maintain your projects</strong>, <strong>develop solutions to existing problems</strong> and provide <strong>analysis</strong>, <strong>review</strong>, 
             <strong>recommendations</strong> (ways forward) and <strong>reporting</strong> on your business technology processes.</p>
-        <p><img src="https://unsplash.com/photos/5fNmWej4tAA"/></p>
+        <p><img src={imageOne}/></p>
         <p>A critical element of the service that we provide is to stay abreast of the rapidly evolving technology world. We stay up-to-date with the most effective and efficient advancements and enhancements
             that come with this change.
         </p>
-        <p><img src="https://unsplash.com/photos/1qkyck-UL3g"/></p>
+        <p><img src={imageTwo}/></p>
         <Script url="https://paperform.co/__embed" />
         <div data-paperform-id="mmgyqlli"></div>
     </div>
