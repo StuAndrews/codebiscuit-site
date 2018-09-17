244dd4c512a57de73b06a7ecc48712cf05854036
Stu Andrews <35388073+StuAndrews@users.noreply.github.com>
2018-09-17 14:49:03 +1000

    More image work
    Cleaning up the images more.

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,11 +4,19 @@ import Script from 'react-load-script';
 import imageOne from './helloquence-61189-unsplash.jpg';
 import imageTwo from './james-pond-185593-unsplash.jpg';
 
-let test = '<script type="text/javascript">(function() { var script = document.createElement("script"); script.src = "https://paperform.co/__embed";document.body.appendChild(script); })()</script>';
-let subTitleStyle = {
+let loadPaperForm = '<script type="text/javascript">(function() { var script = document.createElement("script"); script.src = "https://paperform.co/__embed";document.body.appendChild(script); })()</script>';
+let styleSubTitle = {
     fontStyle: 'italic',
     textAlign: 'center',
 }
+let stylePhotoAttr = {
+    fontStyle: 'italic',
+    textAlign: 'center',
+    fontSize: '14px',
+}
+
+let urlImageOne = 'https://unsplash.com/photos/5fNmWej4tAA';
+let urlImageTwo = 'https://unsplash.com/photos/1qkyck-UL3g';
 
 export default ({data}) => (
     <div>
@@ -18,16 +26,16 @@ export default ({data}) => (
             <meta name="keywords" content="business technology, technology solutions, it projects, systems review, problem solving"/>
         </Helmet>
         <h1>Code Biscuit</h1>
-        <p style={subTitleStyle}>The best tech byte for your business</p>
+        <p style={styleSubTitle}>The best tech byte for your business</p>
         <p>Hi there. Hope you are well.</p>
         <p>At Code Biscuit we provide you with the technology smarts to <strong>achieve your goals</strong>, <strong>realise your dreams</strong> and <strong>turn your ideas into reality</strong>.</p>
         <p>In othing words we help you <strong>build and maintain your projects</strong>, <strong>develop solutions to existing problems</strong> and provide <strong>analysis</strong>, <strong>review</strong>, 
             <strong>recommendations</strong> (ways forward) and <strong>reporting</strong> on your business technology processes.</p>
-        <p><img src={imageOne}/></p>
+        <p style={stylePhotoAttr}><a href={urlImageOne}><img src={imageOne}/></a><br/>Photo by Helloquence</p>
         <p>A critical element of the service that we provide is to stay abreast of the rapidly evolving technology world. We stay up-to-date with the most effective and efficient advancements and enhancements
             that come with this change.
         </p>
-        <p><img src={imageTwo}/></p>
+        <p style={stylePhotoAttr}><a href={urlImageTwo}><img src={imageTwo}/></a><br/>Photo by James Pond</p>
         <Script url="https://paperform.co/__embed" />
         <div data-paperform-id="mmgyqlli"></div>
     </div>
