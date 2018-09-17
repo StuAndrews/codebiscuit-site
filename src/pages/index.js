7f241473d30642bde8c464f937bbd29a7c4a861b
Stu Andrews <35388073+StuAndrews@users.noreply.github.com>
2018-09-17 14:23:55 +1000

    Landing page updated
    Moved the bulk of the text/images into the code, so that the Paperform page is only the Contact Us section. This means the text loads _very_ fast.

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,11 @@ import React from "react"
 import Helmet from 'react-helmet'
 import Script from 'react-load-script'
 
-let test = '<script type="text/javascript">(function() { var script = document.createElement("script"); script.src = "https://paperform.co/__embed";document.body.appendChild(script); })()</script>'
+let test = '<script type="text/javascript">(function() { var script = document.createElement("script"); script.src = "https://paperform.co/__embed";document.body.appendChild(script); })()</script>';
+let subTitleStyle = {
+    fontStyle: 'italic',
+    textAlign: 'center',
+}
 
 export default ({data}) => (
     <div>
@@ -11,8 +15,19 @@ export default ({data}) => (
             <meta name="description" content="The best tech byte for your business"/>
             <meta name="keywords" content="business technology, technology solutions, it projects, systems review, problem solving"/>
         </Helmet>
+        <h1>Code Biscuit</h1>
+        <p style={subTitleStyle}>The best tech byte for your business</p>
+        <p>Hi there. Hope you are well.</p>
+        <p>At Code Biscuit we provide you with the technology smarts to <strong>achieve your goals</strong>, <strong>realise your dreams</strong> and <strong>turn your ideas into reality</strong>.</p>
+        <p>In othing words we help you <strong>build and maintain your projects</strong>, <strong>develop solutions to existing problems</strong> and provide <strong>analysis</strong>, <strong>review</strong>, 
+            <strong>recommendations</strong> (ways forward) and <strong>reporting</strong> on your business technology processes.</p>
+        <p><img src="https://unsplash.com/photos/5fNmWej4tAA"/></p>
+        <p>A critical element of the service that we provide is to stay abreast of the rapidly evolving technology world. We stay up-to-date with the most effective and efficient advancements and enhancements
+            that come with this change.
+        </p>
+        <p><img src="https://unsplash.com/photos/1qkyck-UL3g"/></p>
         <Script url="https://paperform.co/__embed" />
-        <div data-paperform-id="mmgyqlli" data-takeover="1"></div>
+        <div data-paperform-id="mmgyqlli"></div>
     </div>
 );
 
--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,6 +1,30 @@
 import Typography from "typography";
-import lincolnTheme from "typography-theme-lincoln";
 
-const typography = new Typography(lincolnTheme);
+const typography = new Typography({
+    baseFontSize: '18px',
+    baseLineHeight: 1.61,
+    bodyWeight: 300,
+    headerWeight: 700,
+    boldWeight: 700,
+    scaleRatio: 3.5,
+    googleFonts: [
+        { 
+            name: 'Lato',
+            styles: [
+                '300',
+                '300i',
+                '700',
+                '700i',
+            ],
+        },
+    ],
+    headerFontFamily: ['Lato','Helvetica Neue','Helvetica','Roboto','sans-serif'],
+    bodyFontFamily: ['Lato','Helvetica Neue','Helvetica','Roboto','sans-serif'],
+    overrideStyles: ({ adjustFontSizeTo, rhythm }, options, styles) => ({
+        'h1,h2,h3,h4': {
+            textAlign: 'center',
+        },
+    }),
+});
 
 export default typography;
