f8180f1010ead59b4cfcee098e2722a58c20f959
Stu Andrews <35388073+StuAndrews@users.noreply.github.com>
2019-06-22 10:29:03 +1000

    update gatsby latest

--- src/components/footer.js
+++ src/components/footer.js
@@ -16,7 +16,7 @@ const Footer = () => (
                 <div className="footer-text content-main">
                     <div className="footer-segment-left footer-subtitle cb-green-color">The best tech byte for your business</div>
                     <div className="footer-segment-right footer-link"><Link to="privacy">Privacy Statement</Link></div>
-                    <div className="footer-segment-right">&copy; 2018 Code Biscuit</div>
+                    <div className="footer-segment-right">&copy; 2019 Code Biscuit</div>
                 </div>
                 <div className="footer-image"><Img fluid={data.imageFooter.childImageSharp.fluid} alt=""/></div>
             </div>
